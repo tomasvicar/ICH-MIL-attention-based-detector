c31d1aba58c524063d2eae510e8346beccf64297
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-02-22 13:56:46 +0100

    rasave rsna labels

---
+++ preprocess_table_rsna.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+from scipy.io import loadmat
+
+dcom_colection_name = '../rsna_dicomcolection/dicomColl.mat';
+
+
+dcom_colection_name = loadmat(dcom_colection_name)
--- train.py
+++ train.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
             plt.show()
             
         
-        
+        blbost
         
         
         
